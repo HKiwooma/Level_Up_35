45add2f40c4a9249f06420dddb2b7800adc3c656
HarunaIbnYusuf <har.mullah@gmail.com>
2018-08-31 12:39:58 +0300

    create myApp.py and link it to app.py [#160192468]

---
+++ myApp.py
@@ -0,0 +1,7 @@
+
+from myfolder.app import Events
+
+User_Name = (input("Please Enter your Name: > ")).title()
+# - Ask a user to enter one of their names and check through the lists to see
+
+Events(User_Name)
