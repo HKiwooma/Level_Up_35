84aa182a59a3f5abdf1049d55831e2907545d594
HarunaIbnYusuf <har.mullah@gmail.com>
2018-08-31 16:17:51 +0300

    modify myApp.py and link it to reception.py [#160192468]

--- myApp.py
+++ myApp.py
@@ -1,7 +1,7 @@
 
-from myfolder.app import Events
+from reception.reception import registration_checker
 
-User_Name = (input("Please Enter your Name: > ")).title()
+User_Name = (input("Please Enter your Name:\n > ")).title()
 # - Ask a user to enter one of their names and check through the lists to see
 
-Events(User_Name)
+registration_checker(User_Name)
