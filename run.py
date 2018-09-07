34bb6f6e65bbbde3a2fc4e1f3533c2e02622db06
HarunaIbnYusuf <har.mullah@gmail.com>
2018-09-07 19:54:24 +0300

    updates api files [#160348702]

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify
 
 from api.users import Users, users_api
 
-
 app = Flask(__name__)
 app.register_blueprint(users_api)
 
@@ -11,9 +10,8 @@ HOST = '127.0.0.1'
 PORT = 5050
 
 @app.route('/')
-# def hello_word():
-#     return 'Hello World' 
-
+def api():
+    return 'This is the first route' 
 
 if __name__ == '__main__':
     app.run(debug = DEBUG, host = HOST, port = PORT)
